c737297504ba363353781b7c3beb5d8edb69d9b2
feiwang <feiwang@picopi.cn>
2021-08-18 19:38:06 +0800

    调整package

--- app/src/main/java/com/example/feiwangontouch/simple_touch_event_U_model/view/SingleLayout.java
+++ app/src/main/java/com/example/feiwangontouch/simple_touch_event_U_model/view/SingleLayout.java
@@ -5,7 +5,6 @@ import android.util.AttributeSet;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.widget.RelativeLayout;
-
 public class SingleLayout extends RelativeLayout {
     private final String TAG = "SingleLayout";
 
